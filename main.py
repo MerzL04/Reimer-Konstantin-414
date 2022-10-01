5e78433cfa224a0204821f4f6bbae87ebb86d8e1
MerzL04 <113093108+MerzL04@users.noreply.github.com>
2022-10-01 09:46:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Задание №1
+x = 'Hello world!'
+y = 0
+for i in range(len(x)):
+    y+=1
+print(y)
+
+print(x[::-1])
+
+z = 'Hello \'world\'!'
+print(z[7:12])
+
+ 
+#Звдвние №2
+a = '23 456'
+print(a[3:6], a[0:2])
+
+b = 'ivan@lenta.ru'
+print(b [0:4])
+
+c = '7(495) 234-56-78'
+c1 = c.replace("-","")
+print(c1)
+
+
+#Задание №3
+print('Hello world!', sep='')
